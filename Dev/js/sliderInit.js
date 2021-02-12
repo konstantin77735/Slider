0670ef6cd8f4fb71dfac627ebe77b1773918aee2
Konstantin <konstantinstepanyan99@gmail.com>
2021-02-13 00:18:52 +0300

    added the ability to select the number of slides on one screen

---
+++ Dev/js/sliderInit.js
@@ -0,0 +1,36 @@
+//import Slider from './slider.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    let firstSlider = new Slider('.slider', {
+            //currentSlide: 1, // default = 0
+            slidesToShow: 2,
+            speed: 500, //ms, default = 1000
+            //animationType: 'linear', // default = ease
+
+            //autoplay: true, // default = false
+            //autoplayDirection: 'right', //default = right
+            //autoplaySlideTime: 2000, //ms, default = 4000
+
+            //dotsEnable: false, // default = true
+            //arrowsEnable: false, //default = true
+            //dotsHeight: '12px', //default = 20px
+            //dotsWidth: '12px' //default = 20px
+        });
+//    ,    secondSlider = new Slider('.slider2', {
+//            //currentSlide: 1, // default = 0
+//            speed: 3000, //ms, default = 1000
+//            animationType: 'ease', // default = ease
+//
+//            autoplay: true, // default = false
+//            //autoplayDirection: 'left', //default = right
+//            autoplaySlideTime: 3000, //ms, default = 4000
+//
+//            //dotsEnable: false, //default = true
+//            //arrowsEnable: false, //default = true
+//            //dotsHeight: '12px', //default = 20px
+//            //dotsWidth: '12px' //default = 20px
+//        });
+
+});
---
+++ README.md
@@ -0,0 +1,21 @@
+Slider 
+
+
+Slider can move by pressing the left or right arrow
+
+```
+ let firstSlider = new Slider('.slider', {
+        //      currentSlide: 1, по умолчанию 0
+        speed: 1500, //в ms по умолчанию 1000
+        //animationType: 'linear', по умолчанию ease
+
+        //      autoplay: true, по умолчанию false
+        //      autoplayDirection: 'left', //по умолчанию right
+        //      autoplaySlideTime: 4000, //ms
+
+        //      dotsEnable: false, //  По умолчанию true
+        //      arrowsEnable: false, Если не указывать, то по умолчанию будет true
+        //      dotsHeight: '12px',
+        //      dotsWidth: '12px'
+    });
+```
